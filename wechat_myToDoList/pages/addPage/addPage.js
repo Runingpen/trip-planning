cf69f1208613dd1e5f6fde0f86184cbb24938378
CindyJick <2016150155@email.szu.edu.cn>
2018-05-13 09:32:01 +0800

    基本样式的确定
    粗糙地划分了添加任务页面的布局；
    为“过去的任务”界面添加了滑动tab
    
    Co-Authored-By: cindyjick <cindyjick@users.noreply.github.com>

--- wechat_myToDoList/pages/addPage/addPage.js
+++ wechat_myToDoList/pages/addPage/addPage.js
@@ -1,6 +1,5 @@
 var app = getApp()
 Page({
-
   data: {
     array: ['微信信息提醒', '震动提醒','邮件提醒','响铃提醒'],
     objectArray: [
@@ -22,8 +21,10 @@ Page({
       }
     ],
     index: 0,
-    date: '2016-09-01',
-    time: '12:01',
+    startDate: '2016-09-01',
+    startTime: '12:01',
+    endDate: '2018-05-13',
+    endTime: '14:20'
   },
   bindPickerChange: function (e) {
     console.log('picker发送选择改变，携带值为', e.detail.value)
@@ -31,85 +32,28 @@ Page({
       index: e.detail.value
     })
   },
-  bindMultiPickerChange: function (e) {
+  bindStartDateChange: function (e) {
     console.log('picker发送选择改变，携带值为', e.detail.value)
     this.setData({
-      multiIndex: e.detail.value
+      startDate: e.detail.value
     })
   },
-  bindMultiPickerColumnChange: function (e) {
-    console.log('修改的列为', e.detail.column, '，值为', e.detail.value);
-    var data = {
-      multiArray: this.data.multiArray,
-      multiIndex: this.data.multiIndex
-    };
-    data.multiIndex[e.detail.column] = e.detail.value;
-    switch (e.detail.column) {
-      case 0:
-        switch (data.multiIndex[0]) {
-          case 0:
-            data.multiArray[1] = ['扁性动物', '线形动物', '环节动物', '软体动物', '节肢动物'];
-            data.multiArray[2] = ['猪肉绦虫', '吸血虫'];
-            break;
-          case 1:
-            data.multiArray[1] = ['鱼', '两栖动物', '爬行动物'];
-            data.multiArray[2] = ['鲫鱼', '带鱼'];
-            break;
-        }
-        data.multiIndex[1] = 0;
-        data.multiIndex[2] = 0;
-        break;
-      case 1:
-        switch (data.multiIndex[0]) {
-          case 0:
-            switch (data.multiIndex[1]) {
-              case 0:
-                data.multiArray[2] = ['猪肉绦虫', '吸血虫'];
-                break;
-              case 1:
-                data.multiArray[2] = ['蛔虫'];
-                break;
-              case 2:
-                data.multiArray[2] = ['蚂蚁', '蚂蟥'];
-                break;
-              case 3:
-                data.multiArray[2] = ['河蚌', '蜗牛', '蛞蝓'];
-                break;
-              case 4:
-                data.multiArray[2] = ['昆虫', '甲壳动物', '蛛形动物', '多足动物'];
-                break;
-            }
-            break;
-          case 1:
-            switch (data.multiIndex[1]) {
-              case 0:
-                data.multiArray[2] = ['鲫鱼', '带鱼'];
-                break;
-              case 1:
-                data.multiArray[2] = ['青蛙', '娃娃鱼'];
-                break;
-              case 2:
-                data.multiArray[2] = ['蜥蜴', '龟', '壁虎'];
-                break;
-            }
-            break;
-        }
-        data.multiIndex[2] = 0;
-        console.log(data.multiIndex);
-        break;
-    }
-    this.setData(data);
-  },
-  bindDateChange: function (e) {
+  bindEndDateChange: function (e) {
     console.log('picker发送选择改变，携带值为', e.detail.value)
     this.setData({
-      date: e.detail.value
+      endDate: e.detail.value
     })
   },
-  bindTimeChange: function (e) {
+  bindStartTimeChange: function (e) {
     console.log('picker发送选择改变，携带值为', e.detail.value)
     this.setData({
-      time: e.detail.value
+      StartTime: e.detail.value
+    })
+  },
+  bindEndTimeChange: function (e) {
+    console.log('picker发送选择改变，携带值为', e.detail.value)
+    this.setData({
+      EndTime: e.detail.value
     })
   },
   bindRegionChange: function (e) {
